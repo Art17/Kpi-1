417b077e003a79e6be50f6414949b4a011a27212
Art17 <ASaprykin97@gmail.com>
2016-04-17 19:08:15 +0300

    company added

--- courses/prog_base_2/tasks/data_formats/data_formats/director.c
+++ courses/prog_base_2/tasks/data_formats/data_formats/director.c
@@ -1,5 +1,11 @@
 #include "director.h"
 
+typedef struct tagCompany
+{
+    char country[24];
+    char name[24];
+} Company;
+
 struct tagDirector_Private
 {
     char name[24];
@@ -7,6 +13,7 @@ struct tagDirector_Private
     char birthdate[12];
     int year;
     double magic;
+    Company company;
 };
 
 
@@ -53,6 +60,34 @@ int DirectorInitialize(Director* director, xmlDocPtr doc, xmlNodePtr directorNod
             director->dp->magic = magic;
             free(buffer);
         }
+        else if (xmlStrcmp (currentNode->name, "company") == 0)
+        {
+            char* country = xmlGetProp (currentNode, "country");
+            strcpy (director->dp->company.country, country);
+            for (xmlNodePtr currentNode2 = currentNode->xmlChildrenNode;
+                    currentNode2 != NULL;
+                    currentNode2 = currentNode2->next)
+            {
+                if (xmlStrcmp(currentNode2->name, "name") == 0)
+                {
+                    char* buffer = xmlNodeListGetString(doc, currentNode2->xmlChildrenNode, 1);
+                    strcpy (director->dp->company.name, buffer);
+                    free (buffer);
+                }
+            }
+            free (country);
+        }
+        /*else if (!xmlStrcmp(cur->name, (const xmlChar *)"corporation")) {
+            xmlChar * country = xmlGetProp(cur, "country");
+            for(xmlNodePtr cur1 = cur->xmlChildrenNode; cur1 != NULL; cur1 = cur1->next){
+                if(!xmlStrcmp(cur1->name, (const xmlChar *)"name")){
+                    xmlChar * name = xmlNodeListGetString(doc, cur1->xmlChildrenNode, 1);
+                    strcpy(self->corporationData.name, (const char *) name);
+                    free(name);
+                }
+            }
+            strcpy(self->corporationData.country, (const char *) country);
+            free(country);*/
     }
 
     return 0;
@@ -72,6 +107,10 @@ int DirectorPrintData (Director* director)
     printf ("birthdate: \t%s\n", director->dp->birthdate);
     printf ("year:      \t%d\n", director->dp->year);
     printf ("magic:     \t%.2f\n", director->dp->magic);
+
+    printf ("company: \n");
+    printf ("\tcountry: %s\n", director->dp->company.country);
+    printf ("\tname: %s\n", director->dp->company.name);
 
     printf ("\n");
 
